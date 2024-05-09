611517869e181fab830881a7a4024756e247c74d
rihi52 <rileyhire7@gmail.com>
2024-05-08 22:01:52 -0400

    added content to playlist window

--- main/src/main.c
+++ main/src/main.c
@@ -32,6 +32,7 @@ static lv_display_t * hal_init(int32_t w, int32_t h);
 /**********************
  *  STATIC VARIABLES
  **********************/
+const char *playlist_name[] = {"Workout", "Relax", "Study", "Sleep", "Driving", "Party"};
 
 /**********************
  *      MACROS
@@ -56,6 +57,7 @@ int monitor_hor_res = 800, monitor_ver_res = 480;
 static lv_display_t  * disp1;
 static lv_obj_t * scr1;
 static lv_obj_t * list1;
+static lv_obj_t * list2;
 lv_obj_t * window;
 static char *text = "Hello";
 
@@ -77,6 +79,7 @@ static void event_handler_home(lv_event_t * e);
 static void event_handler_browse(lv_event_t * e);
 static void event_handler_search(lv_event_t * e);
 static void event_handler_playlist(lv_event_t * e);
+static void event_handler_choose_playlist(lv_event_t * e);
 static void event_handler_podcast(lv_event_t * e);
 static void event_handler_artists(lv_event_t * e);
 static void event_handler_albums(lv_event_t * e);
@@ -170,39 +173,39 @@ static void main_screen(void)
 
 static void sidebar_list(void)
 {
-    /*Create a list*/
-    static lv_style_t style;
-    lv_style_init(&style);
-    lv_style_set_radius(&style, 0);
+  /*Create a list*/
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_radius(&style, 0);
 
-    list1 = lv_list_create(lv_screen_active());
-    lv_obj_add_style(list1, &style, LV_STATE_DEFAULT);
-    lv_obj_set_size(list1, lv_pct(20), monitor_ver_res);
-    lv_obj_align(list1,LV_ALIGN_LEFT_MID, 0, 0);
+  list1 = lv_list_create(lv_screen_active());
+  lv_obj_add_style(list1, &style, LV_STATE_DEFAULT);
+  lv_obj_set_size(list1, lv_pct(20), monitor_ver_res);
+  lv_obj_align(list1,LV_ALIGN_LEFT_MID, 0, 0);
 
-    /*Add buttons to the list*/
-    lv_obj_t * btn, * btn_playlist, * btn_podcast;
-    btn = lv_list_add_button(list1, LV_SYMBOL_HOME, "Home");
-    lv_obj_add_event_cb(btn, event_handler_home, LV_EVENT_ALL, NULL);
+  /*Add buttons to the list*/
+  lv_obj_t * btn, * btn_playlist, * btn_podcast;
+  btn = lv_list_add_button(list1, LV_SYMBOL_HOME, "Home");
+  lv_obj_add_event_cb(btn, event_handler_home, LV_EVENT_ALL, NULL);
 
-    btn = lv_list_add_button(list1, LV_SYMBOL_EYE_OPEN, "Search");
-    lv_obj_add_event_cb(btn, event_handler_search, LV_EVENT_ALL, NULL);
+  btn = lv_list_add_button(list1, LV_SYMBOL_EYE_OPEN, "Search");
+  lv_obj_add_event_cb(btn, event_handler_search, LV_EVENT_ALL, NULL);
 
-    btn = lv_list_add_button(list1, LV_SYMBOL_DIRECTORY, "Browse");
-    lv_obj_add_event_cb(btn, event_handler_browse, LV_EVENT_ALL, NULL);
+  btn = lv_list_add_button(list1, LV_SYMBOL_DIRECTORY, "Browse");
+  lv_obj_add_event_cb(btn, event_handler_browse, LV_EVENT_ALL, NULL);
 
-    lv_list_add_text(list1, "User");
-    btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Playlists");
-    lv_obj_add_event_cb(btn, event_handler_playlist, LV_EVENT_ALL, NULL);
+  lv_list_add_text(list1, "User");
+  btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Playlists");
+  lv_obj_add_event_cb(btn, event_handler_playlist, LV_EVENT_ALL, NULL);
 
-    btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Podcasts");
-    lv_obj_add_event_cb(btn, event_handler_podcast, LV_EVENT_ALL, NULL);
+  btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Podcasts");
+  lv_obj_add_event_cb(btn, event_handler_podcast, LV_EVENT_ALL, NULL);
 
-    btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Artists");
-    lv_obj_add_event_cb(btn, event_handler_artists, LV_EVENT_ALL, NULL);
+  btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Artists");
+  lv_obj_add_event_cb(btn, event_handler_artists, LV_EVENT_ALL, NULL);
 
-    btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Albums");
-    lv_obj_add_event_cb(btn, event_handler_albums, LV_EVENT_ALL, NULL);
+  btn = lv_list_add_button(list1, LV_SYMBOL_LIST, "Albums");
+  lv_obj_add_event_cb(btn, event_handler_albums, LV_EVENT_ALL, NULL);
 }
 
 /* Sidebar List Button Event Handlers */
@@ -286,6 +289,7 @@ static void home_list(void)
   lv_style_set_border_width(&style_btn, 2);
   lv_style_set_border_color(&style_btn, lv_color_black());
 
+  // Window content
   lv_obj_t * cont = lv_win_get_content(window);
 
   lv_obj_t * btn;
@@ -353,6 +357,35 @@ static void playlist_list(void)
   lv_obj_set_size(window, lv_pct(80), monitor_ver_res);
   lv_obj_align(window,LV_ALIGN_RIGHT_MID, 0, 0);
   lv_win_add_title(window, "Playlists");
+
+  lv_obj_t * cont = lv_win_get_content(window);
+
+  /*Create a list*/
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_radius(&style, 0);
+
+  list2 = lv_list_create(cont);
+  lv_obj_add_style(list2, &style, LV_STATE_DEFAULT);
+  lv_obj_set_size(list2, lv_pct(100), lv_pct(100));
+  lv_obj_align(list2,LV_ALIGN_BOTTOM_MID, 0, 0);
+
+  /*Add buttons to the list*/
+  lv_obj_t * btn, * btn_playlist, * btn_podcast;
+
+  int n = 6;
+  for (int i = 0; i < n; i++){
+    btn = lv_list_add_button(list2, LV_SYMBOL_LIST, playlist_name[i]);
+    lv_obj_add_event_cb(btn, event_handler_choose_playlist, LV_EVENT_ALL, NULL);
+  }
+
+}
+/* Need to go to playlist still */
+static void event_handler_choose_playlist(lv_event_t * e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * obj = lv_event_get_target(e);
+  LV_UNUSED(obj);
 }
 static void artists_list(void)
 {
